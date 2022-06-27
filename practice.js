a5c5f888162d7b2f3ac734edd52de1b2244477ae
Marshnme <jildsmiles@yahoo.com>
2022-06-27 15:33:53 -0400

    capitalize, reverseString,Calculator all tests passing

--- practice.js
+++ practice.js
@@ -1,10 +1,29 @@
-const capitalize = () => {
-	return "Hello";
+const capitalize = (string) => {
+	let splitString = string.split("");
+	splitString[0] = splitString[0].toUpperCase();
+	return splitString.join("");
 };
 
-const reverseString = () => {};
+const reverseString = (string) => {
+	let splitString = string.split("");
+	return splitString.reverse().join("");
+};
 
-const calculator = () => {};
+const calculator = {
+	add: function (numOne, numTwo) {
+		return numOne + numTwo;
+	},
+
+	sub: function (numOne, numTwo) {
+		return numOne - numTwo;
+	},
+	multi: function (numOne, numTwo) {
+		return numOne * numTwo;
+	},
+	divide: function (numOne, numTwo) {
+		return numOne / numTwo;
+	},
+};
 
 const caesar = () => {};
 
--- testPractice.test.js
+++ testPractice.test.js
@@ -9,3 +9,22 @@ import {
 test("returns with first letter capitalized", () => {
 	expect(capitalize("hello")).toMatch("Hello");
 });
+
+test("reverses string", () => {
+	expect(reverseString("hello")).toMatch("olleh");
+});
+
+test("test calculator functions", () => {
+	expect(calculator.add(4, 4)).toBe(8);
+	expect(calculator.sub(4, 3)).toBe(1);
+	expect(calculator.multi(4, 4)).toBe(16);
+	expect(calculator.divide(4, 2)).toBe(2);
+});
+
+// test("returns with first letter capitalized", () => {
+// 	expect(capitalize("hello")).toMatch("Hello");
+// });
+
+// test("returns with first letter capitalized", () => {
+// 	expect(capitalize("hello")).toMatch("Hello");
+// });
